047d4e93aebe631791a1ad59d679722b8160cf75
Ling Evan <lingevan@EMBP-2.local>
2017-08-02 19:28:16 +0100

    init

---
+++ label.py
@@ -0,0 +1,25 @@
+import argparse
+import os
+
+def main(image_dir, annot_dir):
+    # for image in os.scan(image_dir):
+    #     if image.name.endswith('_rgb.jpg'):
+    #         print('opening annotation tool for',image.name)
+    #         newname = image.name.replace('_rgb.jpg','_label.json')
+    #         new_path = os.path.join(annot_dir, newname)
+    #         if os.path.exists(new_path):
+    #             print('Annot for',image.name,'exists, moving on to next image.')
+    #             continue
+    #         os.system('labelme '+image+' -O '+os.path.join(annot_dir,newname))
+    #         print('annotation completed for',imagename)
+    #     else: 
+    #         print('Image format wrong, moving on the next image')
+    print('Finished annotating all images, annotations in',annot_dir,':',
+    len([i in for i in os.listdir(annot_dir) if i.endswith('_label.json')]))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image_dir',help='dir of images to be annotated',type=str)
+    parser.add_argument('annot_dir',help='save directory',type=str)
+    args = parser.parse_args()
+    main(args.image_dir, args.annot_dir)
