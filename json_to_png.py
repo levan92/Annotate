3128dea84a4f670c6e68d5bece17a2572f958b32
Evan Ling <el216@matrix15.doc.ic.ac.uk>
2017-08-06 17:21:35 +0100

    script to convert json to png in batch

---
+++ json_to_png.py
@@ -0,0 +1,28 @@
+import argparse
+import os
+import subprocess 
+
+def main(annot_dir):
+    for json in os.scandir(annot_dir):
+        if not json.name.endswith('_label.json'):
+            continue
+        new_folder = json.path.replace('.','_')
+        if os.path.exists(new_folder):
+            print('png for',json.name,'already exists, continuing next..')
+            continue
+        p = subprocess.Popen(['labelme_json_to_dataset',json.path])
+        try:
+            p.wait()
+        except KeyboardInterrupt:
+            print('\nInterrupted, terminating with progress:',
+                  completed,'/',total)
+            p.terminate()
+            return
+        print(json.name,'converted to png')
+    print('All jsons in dir converted to png.')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('annot_dir',help='save directory',type=str)
+    args = parser.parse_args()
+    main(args.annot_dir)
